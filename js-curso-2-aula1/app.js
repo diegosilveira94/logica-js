7242aa6e9d5d6b04db6aa776fe685bc0258d4dbf
Diego Silveira <diegocilveira@gmail.com>
2025-06-21 16:58:47 -0300

    feat: ativação do botão novo jogo e refatoração das mensagens inicial em função

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1','Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function mensagemInicial() {
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+mensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -18,6 +22,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -37,4 +42,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
