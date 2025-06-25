707583b41174c7d798ae60a3321c0b11eca37d53
Diego Silveira <diegocilveira@gmail.com>
2025-06-24 22:44:04 -0300

    feat: alterando numero limite de escolha para 15

--- js-curso-2 - listas e funções/app.js
+++ js-curso-2 - listas e funções/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 15;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicial() {
     exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 15');
 }
 
 mensagemInicial();
