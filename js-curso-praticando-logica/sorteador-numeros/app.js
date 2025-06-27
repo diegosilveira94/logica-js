3634db85172ff28c2cba05abe21e13b5516c2e77
Diego Silveira <diegocilveira@gmail.com>
2025-06-27 20:04:01 -0300

    fix: sorteador-numeros/app.js tratamento erro caso o intervalo seja maior que o numero inicial entre o final

--- js-curso-praticando-logica/sorteador-numeros/app.js
+++ js-curso-praticando-logica/sorteador-numeros/app.js
@@ -10,7 +10,17 @@ function sortear() {
     document.getElementById('ate').value = '';    
     return;
     }
-    //numeroInicialMaiorQueNumeroLimite(de, ate);
+
+    // caso o intervalo seja maior que o numero inicial e final
+    let diferencaNumeroInicialFinal = ate - de;
+    if(diferencaNumeroInicialFinal < quantidade) {
+        alert('O intervalo é maior entre o número inicial e final.')
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        return;
+    }
+
     // uso da função gerar numero aleatorios
     let sorteados = [];
     let numero;
@@ -24,15 +34,15 @@ function sortear() {
     // inclusão dos numeros aleatorios no elemento resultado
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
-
-    alterarStatusBotao();
+    alterarStatusBotaoSortear()
+    alterarStatusBotaoReiniciar();
 }
 
 function geradorNumeroAleatorio(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function alterarStatusBotao() {
+function alterarStatusBotaoReiniciar() {
     let botao = document.getElementById('btn-reiniciar');
     if (botao.classList.contains('container__botao-desabilitado')) {
         botao.classList.remove('container__botao-desabilitado');
@@ -43,12 +53,24 @@ function alterarStatusBotao() {
     }
 }
 
+function alterarStatusBotaoSortear() {
+    let botao = document.getElementById('btn-sortear');
+    if (botao.classList.contains('container__botao')) {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    } else {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');        
+    }
+}
+
 function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
     document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
-    alterarStatusBotao();
+    alterarStatusBotaoReiniciar();
+    alterarStatusBotaoSortear();
 }
 
 // function numeroInicialMaiorQueNumeroLimite(de, ate) {
